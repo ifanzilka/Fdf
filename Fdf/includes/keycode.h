8a844087acf41c84ec1bc0d5d27ee4c04735f8eb
ifanzilka <nabiyarov.fanzil@gmail.com>
2021-07-11 20:30:49 +0300

    add linux button

--- Fdf/includes/keycode.h
+++ Fdf/includes/keycode.h
@@ -36,22 +36,22 @@
 #  define MINUS 45
 #  define Z 6
 #  define X 7
-#  define W 13
-#  define S 1
-#  define A 0
-#  define D 2
-#  define Q 12
-#  define E 14
+#  define W 119
+#  define S 115
+#  define A 97
+#  define D 100
+#  define Q 113
+#  define E 101
 #  define ESC 65307
-#  define MOUSE_UP 5
+#  define MOUSE_UP 0
 #  define MOUSE_DOWN 4
 #  define MOUSE_LEFT 1
 #  define MOUSE_RIGHT 2
 #  define MOUSE_MIDDLE 3
-#  define DIVIZOR_MINUS 43
-#  define DIVIZOR_PLUS 47
-#  define I 34
-#  define P 35
+#  define DIVIZOR_MINUS 44
+#  define DIVIZOR_PLUS 46
+#  define I 105
+#  define P 112
 # endif
 
 #endif
